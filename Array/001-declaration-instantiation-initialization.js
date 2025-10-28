5d9d07f60b17257e383f20c07005105a6ed8da79
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 11:18:31 +0530

    Creating an array - Declaration, Instantiation & Initialization

---
+++ Array/001-declaration-instantiation-initialization.js
@@ -0,0 +1,11 @@
+// Declaration (declaration only)
+
+let arr1;
+
+// Instantiation (declaration and instantiation)
+
+let arr2 = new Array(5);
+
+// Initialization (declaration, instantiation and initialization)
+
+let arr3 = [10, 20, 30, 40, 50];
