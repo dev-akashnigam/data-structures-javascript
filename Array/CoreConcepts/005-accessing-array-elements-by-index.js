f8e4d70e731f2dbf52e047631f880202ba7ff856
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 12:38:11 +0530

    Accessing/Reading array elements by index

---
+++ Array/CoreConcepts/005-accessing-array-elements-by-index.js
@@ -0,0 +1,19 @@
+let arr = [10, 20, 30, 40, 50];
+
+const firstElement = arr[0];
+const forthElement = arr[3];
+const lastElement = arr[arr.length-1];
+
+const elementAtFirstPlace = arr.at(0);
+const elementAtForthPlace = arr.at(3);
+const elementAtLastPlace = arr.at(-1);
+
+console.log(`First Element of Array = ${firstElement}`);
+console.log(`Forth Element of Array = ${forthElement}`);
+console.log(`Last Element of Array = ${lastElement}`);
+
+console.log(`Element of Array At First Place = ${elementAtFirstPlace}`);
+console.log(`Element of Array At Forth Place = ${elementAtForthPlace}`);
+console.log(`Element of Array At Last Place = ${elementAtLastPlace}`);
+
+
