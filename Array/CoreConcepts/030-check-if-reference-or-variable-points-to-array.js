493d234fd452f442a65bae7d5833b87c6c7bd58e
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-04 00:13:16 +0530

    Determining if a variable is an array

---
+++ Array/CoreConcepts/030-check-if-reference-or-variable-points-to-array.js
@@ -0,0 +1,8 @@
+let arr1 = [10, 20, 30, 40, 50];
+console.log(`Variable arr1 = ${arr1}`);
+
+let arr2 = 10;
+console.log(`Variable arr2 = ${arr2}`);
+
+console.log(`Is variable arr1 an array = ${arr1 instanceof Array}`);
+console.log(`Is variable arr2 an array = ${arr2 instanceof Array}`);
