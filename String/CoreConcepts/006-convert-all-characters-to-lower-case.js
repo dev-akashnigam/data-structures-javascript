3e7d2f3dba4add38fcd3a70d1a585ba16b86ebf8
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-06 13:50:54 +0530

    Convert all characters of string to lower-case

---
+++ String/CoreConcepts/006-convert-all-characters-to-lower-case.js
@@ -0,0 +1,5 @@
+let str = "Hello World";
+console.log(`Original String = ${str}`);
+
+let lowerCaseConvertedString = str.toLowerCase();
+console.log(`After converting string to lower case = ${lowerCaseConvertedString}`);
