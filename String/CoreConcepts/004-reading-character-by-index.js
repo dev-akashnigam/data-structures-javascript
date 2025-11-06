36358ccc93bdb77dcffff7c4aac8db8e7371a8c0
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-06 13:22:41 +0530

    Reading any character of string by index

---
+++ String/CoreConcepts/004-reading-character-by-index.js
@@ -0,0 +1,10 @@
+let str = "Hello World";
+
+const firstChar = str.charAt(0);
+console.log(`First character in string: ${str} = ${firstChar}`);
+
+const fifthChar = str.charAt(4);
+console.log(`Fifth character in string: ${str} = ${fifthChar}`);
+
+const lastChar = str.charAt(str.length-1);
+console.log(`Last character in string: ${str} = ${lastChar}`);
