5945181ba654e16573c31cebf66042e1eb46be9f
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-06 13:42:11 +0530

    Convert all characters of string to upper-case

---
+++ String/CoreConcepts/005-convert-all-characters-to-upper-case.js
@@ -0,0 +1,6 @@
+let str = "Hello World";
+console.log(`Original String = ${str}`);
+
+let upperCaseConvertedString = str.toUpperCase();
+console.log(`After converting string to upper case = ${upperCaseConvertedString}`);
+
