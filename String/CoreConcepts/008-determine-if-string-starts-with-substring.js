77754e70d979ae3d02e7cbbd91b6844e5fa027a6
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-09 12:58:09 +0530

    Determine if a given string starts with a given substring.

---
+++ String/CoreConcepts/008-determine-if-string-starts-with-substring.js
@@ -0,0 +1,9 @@
+const str = "This story is about Akash Nigam";
+console.log(`Original String = ${str}`);
+
+const subs = "This";
+
+const checkStart = str.startsWith(subs);
+
+console.log(`String: "${str}" starts with substring: "${subs}" is = ${checkStart}`);
+
