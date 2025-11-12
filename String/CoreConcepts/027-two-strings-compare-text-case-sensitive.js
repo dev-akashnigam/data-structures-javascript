053c7b2016e81339d405f555ed64a3b072d366d5
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-13 00:19:32 +0530

    Comparing text of two different strings.

---
+++ String/CoreConcepts/027-two-strings-compare-text-case-sensitive.js
@@ -0,0 +1,13 @@
+const str1 = "Life moves fast";
+console.log(`First String = ${str1}`);
+
+const str2 = "Life moves fast";
+console.log(`Second String = ${str2}`);
+
+const areTextSame = str1 === str2;
+
+if(areTextSame) {
+    console.log(`Both the strings have same text.`);
+} else {
+    console.log(`Both the strings have different text.`);
+}
