df37fb5e6bc1d67a45250b56a869dccfe0df2859
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-12 12:00:31 +0530

    Removing leading and trailing whitespaces from a given string.

---
+++ String/CoreConcepts/023-remove-whitespaces-from-string.js
@@ -0,0 +1,5 @@
+let str = "  The presentation outline has been finalized   and approved. ";
+console.log(`Original String = ${str}`);
+
+str = str.trim();
+console.log(`String after removing whitespaces = ${str}`);
