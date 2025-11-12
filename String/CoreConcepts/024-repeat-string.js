87fc4f85db52096c6f19192b83b6320f4757fa9d
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-12 12:25:00 +0530

    Repeating a given string N times.

---
+++ String/CoreConcepts/024-repeat-string.js
@@ -0,0 +1,6 @@
+let str = "Meeting starts soon.";
+console.log(`Original String = ${str}`);
+
+const repeatCount = 3;
+str = str.repeat(repeatCount);
+console.log(`After repeating string ${repeatCount} times, string = ${str}`);
